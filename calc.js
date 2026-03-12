7d268dc6f86397be3f9823876edfbe6a6e5b9699
lokeshwar <lokeshwar18534@gmail.com>
2026-03-12 11:22:04 +0530

    added calc.js

---
+++ calc.js
@@ -0,0 +1,21 @@
+console.log("SIMPLE CALCULATOR")
+let ch = "%"
+let a = 12
+let b = 3
+switch(ch){
+    case '+':
+        console.log('Addition is = ',a+b)
+        break
+    case '-':
+        console.log('Subtraction is = ',a-b)
+        break
+    case '*':
+        console.log('Multiplication is = ',a*b)
+        break
+    case '/':
+        console.log('Division is = ',a/b)
+        break
+    case '%':
+        console.log('Modulus is = ',a%b)
+        break
+}
