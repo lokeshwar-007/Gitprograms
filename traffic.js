22cf32758dd9faa0b3a300833bf97a0e7a7c50bf
lokeshwar <lokeshwar18534@gmail.com>
2026-03-12 11:07:16 +0530

    saved traffic.js

---
+++ traffic.js
@@ -0,0 +1,13 @@
+console.log("Traffic Signal Instructions")
+let light = "green"
+switch(light){
+    case 'red':
+        console.log("Stop the bike until the time ends!")
+        break
+    case 'yellow':
+        console.log("Start the bike and get ready to move!!")
+        break
+    case 'green':
+        console.log("You are ready to go!!!")
+        break
+}
