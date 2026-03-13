4d1423d5de7e69d28b7f067f281b48e0bea54a95
lokeshwar <lokeshwar18534@gmail.com>
2026-03-13 10:06:00 +0530

    saved the program

---
+++ freezingPointCheck.js
@@ -0,0 +1,11 @@
+console.log("TO CHECK THE FREEZING POINT IN CELCIUS")
+function freeze(cel){
+    if (cel == 0){
+        console.log("It is the freezing point in celcius!")
+    }else if(cel > 0){
+        console.log("It is above the freezing point!")
+    }else{
+        console.log("It is below the freezing point!!")
+    }
+}
+freeze(0)
