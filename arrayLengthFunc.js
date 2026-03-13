39bc8d7500e5b354cb6586f0246b72dc787a935c
lokeshwar <lokeshwar18534@gmail.com>
2026-03-13 10:05:17 +0530

    saved the program

---
+++ arrayLengthFunc.js
@@ -0,0 +1,6 @@
+console.log("TO FIND THE LENGTH OF AN ARRAY BY FUNCTIONS")
+function lenFinder(arr){
+    console.log("The length of the given array is : ",arr.length)
+}
+let arr = [1,2,3,4,5,6,7,8]
+lenFinder(arr)
